e1a003ee240e1214ca7c7834a5522568202472d1
Jakesteve102 <jake.fernandezbrizuela@outlook.com>
2023-09-25 15:40:22 -0600

    adding changes

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () =>
 {
-    console.log( process.env.DB_URL );
+    console.log( process.env.MONGO_URI );
     try
     {
-        const connection = await mongoose.connect( `${ process.env.DB_URL }`, {
+        const connection = await mongoose.connect( `${ process.env.MONGO_URI }`, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         } );
